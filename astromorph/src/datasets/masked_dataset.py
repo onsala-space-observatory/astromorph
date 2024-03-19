e16a6ae60d1524ec5e082f57d1b1c317a8b0ae22
Leon Boschman <leon.boschman@chalmers.se>
2024-03-19 16:55:55 +0100

    Make the image conversion methods available for general use

--- astromorph/src/datasets/masked_dataset.py
+++ astromorph/src/datasets/masked_dataset.py
@@ -5,6 +5,8 @@ from astropy.io import fits
 from scipy.ndimage import find_objects, label
 from torch.utils.data import Dataset
 
+from .helpers import augment_image, make_4D 
+
 
 class MaskedDataset(Dataset):
     def __init__(self, datafile: str, maskfile: str):
@@ -65,47 +67,9 @@ class MaskedDataset(Dataset):
         """
         image = self.objects[index]
 
-        # Training the model requires multiple images in a single go, because
-        # this is necessary for the projection in the BYOL architecture.
-        # Since every image has a different size, we do this by creating
-        # multiple copies of each image.
-        # These copies follow the D2 = Z2 x Z2 symmetry group
-
-        im_e = self.make_inferable(image)
-        im_c = np.rot90(im_e, k=2, axes=(2, 3))
-        im_b = np.flip(im_e, axis=(2, 3))
-        im_bc = np.rot90(im_b, k=2, axes=(2, 3))
-
-        # Concatenate along axis 0 to produce a tensor of shape (4, 3, W, H)
-        images = np.concatenate(
-            [
-                im_e,
-                im_c,
-                im_b,
-                im_bc,
-            ],
-            axis=0,
-        )
+        images = augment_image(image)
 
         return torch.from_numpy(images)
-
-    @classmethod
-    def make_inferable(cls, image: np.ndarray):
-        """Produce a version of the image that can be run on the inference network
-
-        Args:
-            image: 2D numpy array
-
-        Returns:
-            4D torch Tensor that can be used for inference
-        """
-        # Clip the most extreme values, and convert to logspace for better
-        # detection of faint features
-        image = np.log10(np.clip(image, a_min=1, a_max=100))
-        # Create two extra dimensions
-        image = np.expand_dims(np.expand_dims(image, axis=0), axis=0)
-        # Create three channels per image (for RGB values)
-        return np.concatenate([image, image, image], axis=1)
 
     def get_all_items(self):
         """Produce all items as inferable images
@@ -113,4 +77,4 @@ class MaskedDataset(Dataset):
         Returns:
             list of 4D torch Tensors that can be used for inference
         """
-        return [self.make_inferable(image) for image in self.objects]
+        return [make_4D(image) for image in self.objects]
