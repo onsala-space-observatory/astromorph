7cc25a49ce6cdcec224efe65bde2caf2f2072f9f
Leon Boschman <leon.boschman@chalmers.se>
2024-04-12 17:13:01 +0200

    Better working version of CloudScanner

--- astromorph/src/models/cloudscanner.py
+++ astromorph/src/models/cloudscanner.py
@@ -5,20 +5,20 @@ from torch import nn
 class CloudScanner(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=7, stride=2, padding=1)
-        self.bn1 = nn.BatchNorm2d(16, eps=1e-5)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(32, eps=1e-5)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1)
 
         self.layer1 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32, eps=1e-5),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32, eps=1e-5),
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(64, eps=1e-5),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
