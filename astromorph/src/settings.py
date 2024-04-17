15f6a486019fc0b1fa3d84dcc05ff7e83a8f24e1
Leon Boschman <leon.boschman@chalmers.se>
2024-04-17 10:46:09 +0200

    Add config file settings for inference

--- astromorph/src/settings.py
+++ astromorph/src/settings.py
@@ -18,9 +18,16 @@ def is_file(filename: str):
 FileName = Annotated[str, AfterValidator(path_exists), AfterValidator(is_file)]
 
 
-class TrainingSettings(BaseModel):
+class FileSettings(BaseModel):
     datafile: FileName
     maskfile: FileName = Field(default='', validate_default=False)
+
+
+class TrainingSettings(FileSettings):
     epochs: int = Field(default=10, gt=0)
     network_name: str
     network_settings: dict
+
+
+class InferenceSettings(FileSettings):
+    trained_network_name: FileName
