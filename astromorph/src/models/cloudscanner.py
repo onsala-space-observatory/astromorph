ba5c91ab742d04f51528300fc9bb48343a412a01
Leon Boschman <leon.boschman@chalmers.se>
2024-04-29 14:47:05 +0200

    Only use single channel for CloudScanner

--- astromorph/src/models/cloudscanner.py
+++ astromorph/src/models/cloudscanner.py
@@ -4,8 +4,8 @@ from torch import nn
 
 class CloudScanner(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=7, stride=2, padding=3)
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=7, stride=2, padding=3)
         self.bn1 = nn.BatchNorm2d(32, eps=1e-5)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1)
