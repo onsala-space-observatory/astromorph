698798a9a2518d293af024f7397a8885671a39a7
Leon Boschman <leon.boschman@chalmers.se>
2024-05-31 13:47:56 +0200

    Add first part of the Learner class.

---
+++ astromorph/src/byol/learner.py
@@ -0,0 +1,101 @@
+from typing import Callable, Optional
+import random
+
+from loguru import logger
+from torch import nn
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms as T
+from tqdm import tqdm
+
+from basic_training import train_single_image
+
+from .byol import BYOL
+
+
+class RandomApply(nn.Module):
+    """A class to provide a probability-layer in a neural network.
+
+    When added as a layer to a neural network, it has probability _p_ to apply
+    function _fn_ to the input. In other cases it will just forward the input.
+
+    Attributes:
+        fn:
+        p:
+    """
+
+    def __init__(self, fn: Callable, p: float):
+        super().__init__()
+        self.fn = fn
+        self.p = p
+
+    def forward(self, x):
+        if random.random() > self.p:
+            return x
+        return self.fn(x)
+
+
+class ByolTrainer(nn.Module):
+
+    DEFAULT_AUGMENTATION_FUNCTION = nn.Sequential(
+        T.RandomHorizontalFlip(),
+        T.RandomRotation(degrees=(0, 360)),
+        RandomApply(T.GaussianBlur((3, 3), (1.0, 2.0)), p=0.2),
+    )
+
+    DEFAULT_OPTIMIZER = torch.optim.Adam
+
+    def __init__(
+        self,
+        network: nn.Module,
+        hidden_layer: str = "avgpool",
+        representation_size: int = 128,
+        augmentation_function: Optional[Callable] = None,
+        learning_rate: float = 5.0e-6,
+        **kwargs
+    ) -> None:
+        super().__init__()
+
+        self.augmentation_function = (
+            augmentation_function
+            if augmentation_function is not None
+            else self.DEFAULT_AUGMENTATION_FUNCTION
+        )
+
+        self.byol = BYOL(
+            network=network,
+            hidden_layer=hidden_layer,
+            augmentation_function=self.augmentation_function,
+            representation_size=representation_size,
+        )
+
+        self.optimizer = self.DEFAULT_OPTIMIZER(
+            self.byol.parameters(), lr=learning_rate
+        )
+
+    def forward(self, x):
+        return self.byol.forward(x, return_embedding=True, return_projection=False)
+
+    def train_epoch(self, train_data: DataLoader, batch_size: int = 16):
+        total_loss = 0.0
+        batch_loss = None
+
+        for i, image in enumerate(tqdm(train_data)):
+            image = image[0]
+            loss = self.train_single_image(image)
+
+            batch_loss = batch_loss + loss if batch_loss else loss
+            total_loss += loss.sum()
+
+            if i % batch_size == 0 and i > 0:
+                batch_loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                self.byol.update_moving_average()
+                batch_loss=None
+
+        return total_loss
+
+    def train_single_image(self, image):
+        return self.byol(image)
+
